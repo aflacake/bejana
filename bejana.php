28c782fe6e026358393d4bb4030b129e9f39ff4c
Nazwa Shabrina <161173839+aflacake@users.noreply.github.com>
2024-09-09 14:39:32 +0700

    Update bejana.php
    Posisi "//" terbaca garis kode php serta pesan lainnya

--- bejana.php
+++ bejana.php
@@ -1,5 +1,6 @@
-//contoh bejana Nabi Ilyas
 <?php
+#kode php versi 8.3
+//judul: bejana Nabi Ilyas
 $wadah = "bejana";
 $sesuatu = array("air", "tepung", "buah-buahan");
   echo "$wadah didalamnya ada $sesuatu</br>";
