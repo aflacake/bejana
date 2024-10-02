e5d58520d83a8a40c2f6f39b6a5374f7034d3604
Nazwa Shabrina <161173839+aflacake@users.noreply.github.com>
2024-10-02 09:20:52 +0700

    Create tes.php
    1

---
+++ if else/tes.php
@@ -0,0 +1,16 @@
+<?php
+// volume benda
+$a = 3; //1-3 ukuran
+$b = 4; //1-7 ukuran
+ echo "maka,";
+
+if ($a < "2"); {
+   echo "<strong>cukup terisi<strong>";
+} 
+  elseif ($a < "4"); {
+  echo "4";
+} 
+else {
+  echo "4";
+}
+?>
