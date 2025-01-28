a65aec1b74820702739858744ad37e3663a07d24
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-01-28 20:37:16 +0700

    Update tes.php

--- if else/tes.php
+++ if else/tes.php
@@ -1,10 +1,10 @@
 <?php
 // volume benda
-$a = 3; //1-3 ukuran
-$b = 4; //1-7 ukuran
+$bejana1 = 2; //1-3 ukuran
+$bejana2 = 4; //1-7 ukuran
  echo "maka, </br>";
 
-if ($a < "2"); {
-   echo "<strong>cukup terisi<strong>";
+if ($bejana1 < "2"); {
+   echo "<strong>cukup terisi</strong>";
 } 
 ?>
