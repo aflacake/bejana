e2ab7b3cbbb2c1a1d611924ea59a82026ac668cc
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-04-30 20:24:26 +0700

    Membuat method_missing.rb
    Untuk kata yang tidak terkait di module, luar module

---
+++ modules/method_missing.rb
@@ -0,0 +1,29 @@
+# method_missing.rb
+
+class Wadah
+  def initilize
+    @data = {}
+    @actions = []
+  end
+
+  def method_missing(name, *args, &block)
+    if block_given?
+      puts "Block untuk #{name}"
+      @actions << { name: name, block: block }
+    else
+      @data[name] = args.first
+    end
+  self
+end
+
+def jalankan
+  @actions.each do |a|
+    puts "Menjalankan #{a[:name]}"
+    instance_eval(&a[:block])
+  end
+end
+
+def tampilan(isi = nil)
+  puts isi || @data.inspect
+  end
+end
