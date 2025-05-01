2eb07a3cb13ef86682e71740aab02882e2c5c523
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-01 18:49:14 +0700

    Perubahan akibat env
    Memperluas dengan pengendalian fie, yang nantinya di `core.rb`. Gar juga peruabhan ini dapat diakses di plugin dan menerima `context.env`nya. Ngomong-ngomong induk komitmen adalah ini.

--- main.rb
+++ main.rb
@@ -3,12 +3,15 @@
 require 'yaml'
 
 require_relative 'core'
+
+require_relative 'core'
 require_relative 'modules/output'
 require_relative 'modules/logika'
 require_relative 'modules/input'
 require_relative 'modules/selain_jika'
 require_relative 'modules/selama'
 require_relative 'modules/berhenti_jika'
+require_relative 'modules/environment_modul'
 
 class BejanaApp < Bejana::Wadah
   include Bejana::FungsiOutput
@@ -17,6 +20,8 @@ class BejanaApp < Bejana::Wadah
   include Bejana::FungsiLogika::SelainJika
   include Bejana::FungsiLogika::Selama
   include Bejana::FungsiLogika::BerhentiJika
+
+ attr_reader :env
 end
 
 files = Dir.glob("*.bjn") + Dir.glob("*.gnuc")
@@ -25,7 +30,7 @@ if files.empty?
   puts"Tidak ada file .bjn atau .gnuc ditemukan"
   exit
 elsif files.size == 1
-  bjn_file = file.first
+  bjn_file = files.first
 else
   puts "Pilih file.bjn yang ingin dijalankan"
   files.each.with_index { |f, i| puts"#{i + 1}.#{f}" }
@@ -45,14 +50,19 @@ when ".gnuc"
   puts kode
 else
   puts"Ekstensi file tidak dikenali"
+  exit
 end
 
 Dir["./plugins/*.rb"].each { |file| require file }
 
-config = YAML.load_file('config.yaml')
+if File.exists?('config.yaml')
+  config = YAML.load_file('config.yaml')
 
-config["plugins"].each do |plugin_name|
-  require_relative "./plugins/#{plugin_name}"
-  plugin_module = Object.const_get(plugin_name.split('_').map(&:capitalize).join)
-  plugin_module.run(context)
+  if config["plugins"]
+    config["plugins"].each do |plugin_name|
+      require_relative "./plugins/#{plugin_name}"
+      plugin_module = Object.const_get(plugin_name.split('_').map(&:capitalize).join)
+      plugin_module.run(context) if context
+    end
+  end
 end
