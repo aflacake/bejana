1362d0a4cfd9ad8c484ec8fe9c8421c533a99651
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-07 08:11:45 +0700

    Membuat basis_data.rb
    Mencari data berdasarkan kriteria tertentu. Melakukan pencarian atau _query_ data menggunakan metode `query` pada kelas `Database`.

---
+++ modules/basis_data.rb
@@ -0,0 +1,19 @@
+class BasisData
+  def initialize
+    @data = []
+  end
+
+  def tambah(rekam)
+    @data << rekam
+  end
+
+  def cari(kriteria)
+    @data.select do |rekam|
+      kriteria.all? { |key, value| rekam[key] == value }
+    end
+  end
+
+  def semua
+    @data
+  end
+end
