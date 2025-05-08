bb6f317fa14ae463aeb9e20a0a58e88a96e9d665
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-08 19:18:17 +0700

    Membuat navigator_modul.rb

---
+++ modules/navigator_modul.rb
@@ -0,0 +1,24 @@
+# modules/navigator_modul.rb
+
+module Bejana
+  module NavigatorModul
+    class Navigator
+      def initialize(content = nil)
+        @langkah = {}
+        @current = nil
+        @context = context
+      end
+
+      def tambah(nama, &blok)
+        @langkah[nama.to_sym] = blok;
+       end
+
+      def mulai_dari(nama)
+        @current = nama.to_sym
+        while @current && @langkah[@current]
+          @current = @context.instance_eval(&@langkah[@current])
+        end
+      end
+    end
+  end
+end
