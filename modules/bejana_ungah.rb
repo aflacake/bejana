86c059efa07d58c714bbfec80e320034262f4cb4
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-09 11:46:20 +0700

    Membuat bejana_ungah.rb
    Mejnadikan Bejana dapat langsung di unggah dengan aman ke server dengan versi bawaannya, namun versi keamanan peningkatan mungkin di perubahan selanjutnya. Selain itu komitmen ini membuat dampak di `README.md`.

---
+++ modules/bejana_ungah.rb
@@ -0,0 +1,28 @@
+require 'httparty'
+require 'json'
+
+class BejanaApp
+  def unggah_file_bjn(bjn_file)
+    print "Masukkan URL server untuk menggunggah file:"
+    url. gets.chomp.strip
+
+    unless File.exist?(bjn_file)
+      puts "File tidak ditemukan!"
+      return
+    end
+
+    file_data = File.read(bjn_file)
+
+    response = HTTParty.post(url,
+                              body: {
+                                file: File.new(bjn_file, 'rb')
+                              },
+                              headers: { 'Content-Type => 'multipart/form-data' })
+
+    if response.success?
+      puts "File berhasil di unggah!"
+    else
+      puts "Terjadi kesalahan saat mnggunggah file: #{response.message}"
+    end
+  end
+end
