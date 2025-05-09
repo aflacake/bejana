0b0c7b41f92b74f453c7ad02a6a6d9d397920886
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-05-09 11:29:25 +0700

    Membuat konversi_json.rb
    Cek list di komitmen saat ini sudah dikerjakan dengan baik, dc44777. Untuk fitur baru ditambahkan agar dapat diunggah ke server.

---
+++ modules/konversi_json.rb
@@ -0,0 +1,32 @@
+require 'json'
+
+def konversi_bjn_ke_json(bjn_file)
+  data = {}
+
+  File.readlines(bjn_file).each_with_index do |baris, i|
+    baris.strip!
+    next if baris.empty? || baris.start_with('#')
+
+    case baris
+    when /^isi (\w+)\s+"?(.*?)"?$/
+      kunci = $1
+      nilai = $2
+      data[kunci] = nilai.match(/^\d+$/) ? nilai.to_i : nilai
+    when /^cetak "(.*?)"$/
+      teks = $1
+      puts teks.gsub(/{{(.?*)}}/) { data[$1.strip.to_sym] }
+    else
+      puts "Perintah tidak dikenali di baris #{i + 1}: #{baris}"
+    end
+  end
+
+  json_data = data.to_json
+
+  File.open("file.json", "w") do |file|
+    file.write(json_data)
+  end
+
+  puts "File JSON telah diekspor ke output.json"
+end
+
+konversi_bjn_ke_json('script.bjn')
