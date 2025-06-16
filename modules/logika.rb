6d5220030f65ffedd3295e9caab508323b4c41b3
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-16 13:08:34 +0700

    Memperbaiki typo dan mengeluarkan module namun beberapa
    Typo penulisan di daerah `jika_tidak` dll. Sebaliknya seperti `berhenti_jika` fungsinya di keluarkan, serta juga dengan `selain_jika`.

--- modules/logika.rb
+++ modules/logika.rb
@@ -2,6 +2,14 @@
 
 module Bejana
   module FungsiLogika
+    module Selama
+      def selama(kunci, &kondisi)
+        while kondisi.call(@data[kunci.to_sym])
+          yield if block_given?
+        end
+      end
+    end
+
     def initialize(&block)
       super(&block)
       @fungsi = {}
@@ -19,7 +27,7 @@ module Bejana
         blok = context[:block]
 
         old_data = @data.dup
-        params.each_with_indexdo |param, index|
+        params.each_with_index do |param, index|
           @data[param.to_sym] = args[index]
         end
 
@@ -31,18 +39,19 @@ module Bejana
       end
     end
 
-    def jika(kunci, kondisi)
+    def jika(kunci, kondisi, &block)
       nilai = @data[kunci.to_sym]
       hasil = kondisi.call(nilai)
 
       if hasil
         instance_eval(&block)
-      elsif block_given? && block.responnd to?(:else_block)
+      elsif block_given? && block.respond_to?(:else_block)
         instance_eval(&block.else_block)
       end
     end
 
     def jika_tidak
       yield if @last_jika == false && block_given?
+    end
   end
 end
