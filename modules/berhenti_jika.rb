1764ee47d706c33f6f62cdd96c7ebc3348f2d379
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-16 13:04:55 +0700

    Membuat berhenti_jika.rb
    Membuat ulang.

---
+++ modules/berhenti_jika.rb
@@ -0,0 +1,13 @@
+# modules/berhenti_jika.rb
+
+module Bejana
+  module FungsiLogika
+    module BerhentiJika
+      def berhenti_jika(kunci, &kondisi)
+        if kondisi.call(@data[kunci.to_sym])
+          throw :berhenti
+        end
+      end
+    end
+  end
+end
