569e59706fb244b9e0f35105b7a183a4af503bbe
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-30 13:02:42 +0700

    Membuat bejana.rb
    Membuat file `bejana.rb` di root proyek file folder. CLI ini akan menjadi antarmuka yang menjadi memanggil fungsionalitas `Bejana` seperti: menjalankan file `.bjn`, menyimpan atau memuat data, menampilkan variabel, dan menjalankan langkah dari interpreter.

---
+++ bejana.rb
@@ -0,0 +1,36 @@
+# bejana.rb
+
+#!/usr/bin/env ruby
+require 'json'
+require_relative 'main'
+
+perintah = ARGV[0]
+namafile = ARGV[1]
+
+case perintah
+when "jalankan"
+  if namafile && File.exist?(namafile)
+    puts "Menjalankan #{namafile}..."
+    system("ruby main.rb #{namafile}")
+  else
+    puts "File tidak ditemukan"
+  end
+when "muat"
+  data = JSON.parse(File.read("bejana_data.json"))
+  puts "Data yang dimuat:"
+  data.each { |k, v| puts "#{k}: #{v}" }
+when "hapus_data"
+  File.delete("bejana_data.json") if File.exist?("bejana_data.json")
+  puts "Data yang disimpan dihapus"
+when "bantuan"
+  puts <<~HELP
+    Perintah CLI Bejana:
+    ---------------------
+    bejana.rb jalankan <file.bjn>  # Menjalankan file Bejana
+    bejana.rb muat                 # Menampilkan isi bejana_data.json
+    bejana.rb hapus_data           # Menghapus data tersimpan
+    bejana.rb bantuan              # Menampilkan bantuan
+  HELP
+else
+  puts "Perintah tidak dikenali. Gunakan 'bantuan' untuk melihat daftar perintah."
+end
