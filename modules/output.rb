38061de7f6696238b1c09846198b78abb088d118
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-06-30 12:09:42 +0700

    `@env` sebagai penyimpanan utama
    Akses diubah, maka `cetak` harus diubah ke akses `@env.get` bukan `@data`.

--- modules/output.rb
+++ modules/output.rb
@@ -3,7 +3,7 @@
 module Bejana
   module FungsiOutput
     def cetak(teks)
-    puts teks.gsub(/{{(.*?)}}/) { |m| @data[$1.strip.to_sym].to_s }
+      puts teks.gsub(/{{(.*?)}}/) { |m| @env.get($1.strip.to_sym).to_s }
     end
   end
 end
