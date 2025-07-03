8f619e0bc5775a83066a33b18627bf12b951e0c3
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-07-03 07:02:33 +0700

    Penemuan BOM
    BOM ditemukan maka dihapus karena oleh kode, `if kode.bytes[0, 3] == [0xEF, 0xBB, 0xBF]`. Jika BOM ditemukan, maka tiga byte pertama akan dihapus dengan `kode = kode[3..-1]`.

--- jalankan_bejana.rb
+++ jalankan_bejana.rb
@@ -8,6 +8,12 @@ filename = ARGV[0] || 'script.bj'
 
 if File.exist?(filename)
   kode = File.read(filename)
+
+  if kode.bytes[0, 3] == [0xEF, 0xBB, 0xBF]
+    kode = kode[3..-1]
+    puts "BOM ditemukan dan dihapus."
+  end
+
   puts "Kode yang dibaca dari file #{filename}:"
   puts kode
 
@@ -24,4 +30,3 @@ if File.exist?(filename)
 else
   puts "File #{filename} tidak ditemukan"
 end
-
