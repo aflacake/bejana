1e6722b5b7af3d9ce579e93905eb11e2cfccbd9a
Nazwa Shabrina Zain <161173839+aflacake@users.noreply.github.com>
2025-07-10 11:38:11 +0700

    _require_relative_ dari `errors.rb`
     _require_ di file lain yang dibutuhkan, agar struktur proyek tetap rapi dan modular.

--- bejana_interpreter.rb
+++ bejana_interpreter.rb
@@ -6,6 +6,7 @@ require 'yaml'
 
 require_relative 'modules/navigator_modul'
 require_relative 'modules/crud_modul'
+require_relative 'errors'
 
 class BejanaInterpreter
   include Bejana::FungsiCRUD
